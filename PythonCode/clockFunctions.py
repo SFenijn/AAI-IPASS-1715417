9461c5068e5d887e1e3b4947f6f618fbdc5d007c
SFenijn <stijn.fenijn@student.hu.nl>
2019-06-25 17:04:02 +0200

    working suggestion for action, now also to earlier in the day.

--- PythonCode/clockFunctions.py
+++ PythonCode/clockFunctions.py
@@ -5,6 +5,13 @@
 import time
 from datetime import timedelta
 
+
+def timeToint(t):
+    day_h = t.days * 24
+    rest_h = t.seconds /60 /60
+    return day_h + rest_h
+
+
 day = timedelta(hours=27)
 hour = timedelta(hours=75)
 dayhour = day.days
@@ -16,3 +23,21 @@ print(newtime)
 print(dayint)
 hours = dayhour * 24
 print(dayint + hours)
+print("--------------------------------")
+
+predicted = timedelta(hours=27)
+target = timedelta(hours=22)
+change = timedelta(hours=1.5)
+plus = predicted + change
+min = predicted - change
+print(plus)
+print(min)
+diff_p = abs(timeToint(plus) - timeToint(target))
+diff_m = abs(timeToint(min) - timeToint(target))
+print(diff_p)
+print(diff_m)
+if diff_p < diff_m:
+    actiontime = plus
+else:
+    actiontime = min
+print(actiontime)
